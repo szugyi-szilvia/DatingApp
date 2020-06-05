3229b29406e8d3ede89056b80e7171df0220c28c
szilvia <szugyisz@yahoo.com>
2020-06-05 18:36:37 +0200

    end of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +11,29 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfuly');
+      //console.log('Logged in successfuly');
+      this.alertify.success('Logged in successfuly');
     }, error => {
-      console.log(error);
+      //console.log(error);
+      this.alertify.error(error);
     });
   }
 
-loggedIn(){
-  const token = localStorage.getItem('token');
-  return !!token; // return true or false
-}
-logout(){
-  localStorage.removeItem('token')
-  console.log('logged out');
-}
+  loggedIn() {
+  return this.authService.loggedIn();
+
+  }
+
+  logout(){ 
+    localStorage.removeItem('token')
+    this.alertify.message('logged out');
+  }
 
 }
